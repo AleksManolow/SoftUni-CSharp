2ace5de5bd46ffe92a6affbc3a07b6372fbeb9fa
AleksManolow <amanolov7@gmail.com>
2022-07-08 11:31:40 +0300

    New changes

---
+++ C#Fundamentals/week06_Objects and Classes/Exercise/task01_Advertisement Message/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task01_Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
