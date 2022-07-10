f13d3cf9d0f88628afca457317c847c44a38012e
AleksManolow <amanolov7@gmail.com>
2022-07-10 18:11:15 +0300

    Add tasks from exercise

---
+++ C#Fundamentals/week07_Associative Arrays/Exercise/task01_Count Chars in a String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01_Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> numberOfOccurrencesOfLetters = new Dictionary<char, int>();
+            string[] words = Console.ReadLine().Split();
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (!numberOfOccurrencesOfLetters.ContainsKey(word[i]))
+                    {
+                        numberOfOccurrencesOfLetters.Add(word[i], 0);
+                    }
+                    numberOfOccurrencesOfLetters[word[i]]++;
+                }
+            }
+
+            foreach (var item in numberOfOccurrencesOfLetters)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/week07_Associative Arrays/Exercise/task02_A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task02_A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> collection = new Dictionary<string, int>();
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!collection.ContainsKey(resource))
+                {
+                    collection.Add(resource, 0);
+                }
+                collection[resource] += quantity;
+            }
+            foreach (var item in collection)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
