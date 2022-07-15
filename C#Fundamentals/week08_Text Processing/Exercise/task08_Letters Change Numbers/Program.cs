2704fb5559ef3c5dbbf10f927b503be5e729605c
AleksManolow <amanolov7@gmail.com>
2022-07-15 14:40:12 +0300

    Add task08_Letters_Change_Numbers

---
+++ C#Fundamentals/week08_Text Processing/Exercise/task08_Letters Change Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task08_Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            double result = 0;
+            foreach (var item in input)
+            {
+                double number = int.Parse(item.Substring(1, item.Length - 2));
+                if (char.IsUpper(item[0]) && char.IsUpper(item[item.Length - 1]))
+                {
+                    number /= ((int)item[0] - 64);
+                    number -= ((int)item[item.Length - 1] - 64);
+
+                }
+                else if (char.IsUpper(item[0]) && char.IsLower(item[item.Length - 1]))
+                {
+                    number /= ((int)item[0] - 64);
+                    number += ((int)item[item.Length - 1] - 96);
+                }
+                else if (char.IsLower(item[0]) && char.IsUpper(item[item.Length - 1]))
+                {
+                    number *= ((int)item[0] - 96);
+                    number -= ((int)item[item.Length - 1] - 64);
+                }
+                else if (char.IsLower(item[0]) && char.IsLower(item[item.Length - 1]))
+                {
+                    number *= ((int)item[0] - 96);
+                    number += ((int)item[item.Length - 1] - 96);
+                }
+                result += number;
+            }
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
