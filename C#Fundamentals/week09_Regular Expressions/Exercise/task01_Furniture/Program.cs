142f432b86ad92471b15f8b57a7ff389b22733ae
AleksManolow <amanolov7@gmail.com>
2022-07-16 16:50:27 +0300

    Add task01 and task03 from week09

---
+++ C#Fundamentals/week09_Regular Expressions/Exercise/task01_Furniture/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace task01_Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[>]{2}(?<name>[A-Za-z]+)[<]{2}(?<price>\d+(.\d+)?)\!(?<quantity>\d+)";
+            string input = Console.ReadLine();
+
+            List<string> names = new List<string>();
+            double totalPrice = 0;
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    names.Add(match.Groups["name"].Value);
+                    totalPrice += (double.Parse(match.Groups["price"].Value) * double.Parse(match.Groups["quantity"].Value));
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+        }
+    }
+}
---
+++ C#Fundamentals/week09_Regular Expressions/Exercise/task03_SoftUni Bar Income/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace task03_SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%(?<customer>[A-Z][a-z]+)%[^%$|.]*?<(?<product>\w+)>[^%$|.]*?\|(?<count>\d+)\|[^%$|.]*?(?<price>\d+(.\d+)?)\$";
+            string input = Console.ReadLine();
+
+            double totalPrice = 0;
+            while (input != "end of shift")
+            {
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    Console.WriteLine($"{match.Groups["customer"].Value}: " +
+                        $"{match.Groups["product"].Value} - " +
+                        $"{(int.Parse(match.Groups["count"].Value) * double.Parse(match.Groups["price"].Value)):F2}");
+                    totalPrice += (int.Parse(match.Groups["count"].Value) * double.Parse(match.Groups["price"].Value));
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalPrice:F2}");
+        }
+    }
+}
