cc71373a0be20bc279cb60b89e11e40c851a96f9
AleksManolow <amanolov7@gmail.com>
2022-07-28 13:47:09 +0300

    Add task06 from Exercise

---
+++ C#Fundamentals/week09_Regular Expressions/Exercise/task06_Extract Emails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace task06_Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|\s)[A-Za-z0-9][\w*\.\-]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]*)+\b";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            matches.ToList().ForEach(Console.WriteLine);
+
+
+        }
+    }
+}
