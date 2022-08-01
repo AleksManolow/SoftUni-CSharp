86d90fa50934c056b694c02875bce7093a2f411d
AleksManolow <amanolov7@gmail.com>
2022-08-01 14:52:57 +0300

    Add task02 from Exam Preparation Exercise

---
+++ C#Fundamentals/Final Exam Preparation/Exam Preparation Exercise/task02_Emoji Detector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace task02_Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattternCoolThreshold = @"\d";
+            string patternEmojis = @"([:|*])\1[A-Z][a-z]{2,}\1\1";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matchesCoolThreshold = Regex.Matches(text,pattternCoolThreshold);
+            MatchCollection matchesEmojis = Regex.Matches(text,patternEmojis);
+            int coolThreshold = 1;
+            foreach (Match item in matchesCoolThreshold)
+            {
+                coolThreshold *= int.Parse(item.ToString());
+            }
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{matchesEmojis.Count} emojis found in the text. The cool ones are:");
+            foreach (Match emoji in matchesEmojis)
+            {
+                string tempEmoji = emoji.ToString();
+                int sumCharEmoji = 0;
+                for (int i = 2; i < tempEmoji.Length - 2; i++)
+                {
+                    sumCharEmoji += (int)tempEmoji[i];
+                }
+
+                if (sumCharEmoji >= coolThreshold)
+                {
+                    Console.WriteLine($"{tempEmoji}");
+                }
+            }
+        }
+    }
+}
