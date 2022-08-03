8e19687cab5672ddd4e57c083813b531aaa0787a
AleksManolow <amanolov7@gmail.com>
2022-08-03 13:07:40 +0300

    Add 02. Programming Fundamentals Final Exam

---
+++ C#Fundamentals/Final Exam Preparation/02. Programming Fundamentals Final Exam/task01_World Tour/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task01_World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+            string[] input = Console.ReadLine().Split(':');
+            while (input[0] != "Travel")
+            {
+                if (input[0] == "Add Stop" && int.Parse(input[1]) >= 0 && int.Parse(input[1]) < stops.Length)
+                {
+                    stops = stops.Substring(0, int.Parse(input[1])) + input[2] + stops.Substring(int.Parse(input[1]));
+                }
+                else if (input[0] == "Remove Stop" && int.Parse(input[1]) >= 0 && int.Parse(input[1]) < stops.Length
+                    && int.Parse(input[2]) >= 0 && int.Parse(input[2]) < stops.Length)
+                {
+                    stops = stops.Remove(int.Parse(input[1]), int.Parse(input[2]) - int.Parse(input[1]) + 1);
+                }
+                else if (input[0] == "Switch")
+                {
+                    stops = stops.Replace(input[1], input[2]);
+                }
+                Console.WriteLine(stops);
+                input = Console.ReadLine().Split(':');
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
---
+++ C#Fundamentals/Final Exam Preparation/02. Programming Fundamentals Final Exam/task02_Destination Mapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace task02_Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"([=|\/])[A-Z][A-z]{2,}\1";
+            int pointToTravel = 0;
+
+            List<string> destinations = new List<string>();
+            MatchCollection matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                destinations.Add(match.ToString().Substring(1, match.ToString().Length - 2));
+                pointToTravel += match.ToString().Substring(1, match.ToString().Length - 2).Length;
+            }
+            
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {pointToTravel}");
+        }
+    }
+}
