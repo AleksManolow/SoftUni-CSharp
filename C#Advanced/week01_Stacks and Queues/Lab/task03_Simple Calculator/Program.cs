3f04447d24d5df39a730aab97d1b5cff94ec2708
AleksManolow <amanolov7@gmail.com>
2022-09-13 16:39:50 +0300

    Add task03 Simple Calculator

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task03_Simple Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task03_Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Stack<string> calcStack = new Stack<string>(input.Reverse());
+            int resultCalc = int.Parse(calcStack.Pop());
+
+            while (calcStack.Count > 0)
+            {
+                string temp = calcStack.Pop();
+                if (temp == "+")
+                {
+                    resultCalc += int.Parse(calcStack.Pop());
+                }
+                else if (temp == "-")
+                {
+                    resultCalc -= int.Parse(calcStack.Pop());
+                }
+            }
+            Console.WriteLine(resultCalc);
+        }
+    }
+}
