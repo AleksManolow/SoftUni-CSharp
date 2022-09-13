420d7ec4455380d7882637bd4a544bd2a24e8671
AleksManolow <amanolov7@gmail.com>
2022-09-13 15:25:44 +0300

    Add task01

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task01_Reverse a String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01_Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reverseString = new Stack<char>();
+            foreach (char c in input)
+            {
+                reverseString.Push(c);
+            }
+
+            while (reverseString.Count > 0)
+            {
+                Console.Write(reverseString.Pop());
+            }
+        }
+    }
+}
