18617e1740a00e839291c0b68f78bf4d2027e824
AleksManolow <amanolov7@gmail.com>
2022-09-13 16:46:40 +0300

    Add task04 Matching Brachets

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task04_Matching Brackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task04_Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mathExpresion = Console.ReadLine();
+            Stack<int> bracketIndex = new Stack<int>();
+            for (int i = 0; i < mathExpresion.Length; i++)
+            {
+                if(mathExpresion[i] == '(')
+                {
+                    bracketIndex.Push(i);
+                }
+                else if (mathExpresion[i] == ')')
+                {
+                    int startIndex = bracketIndex.Pop();
+                    Console.WriteLine(mathExpresion.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
