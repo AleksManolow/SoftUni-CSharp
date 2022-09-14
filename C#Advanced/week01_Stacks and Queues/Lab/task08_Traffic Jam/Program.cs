9af55276e86bae6d8afeaddd8f478c4c298b6576
AleksManolow <amanolov7@gmail.com>
2022-09-14 15:10:13 +0300

    Add task08_Traffic Jam

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task08_Traffic Jam/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task08_Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> vehicles = new Queue<string>();
+            string input = Console.ReadLine();
+            int totalCarsPassed = 0;
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < n && vehicles.Count > 0; i++)
+                    {
+                        Console.WriteLine($"{vehicles.Dequeue()} passed!");
+                        totalCarsPassed++;
+                    }
+                }
+                else
+                {
+                    vehicles.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+        }
+    }
+}
