4fea5bd314cc69afb492c4c9290b9704d93e1059
AleksManolow <amanolov7@gmail.com>
2022-09-14 12:53:59 +0300

    Add task05-Print Even Numbers

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task05_Print Even Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task05_Print_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> elements = new Queue<int>(numbers.Where(x => x % 2 == 0).ToArray());
+            Console.WriteLine(string.Join(", ", elements));
+        }
+    }
+}
