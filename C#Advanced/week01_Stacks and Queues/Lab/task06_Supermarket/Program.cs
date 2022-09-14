998eb2be22bc8f152048190de7cc1a5fa8e449f2
AleksManolow <amanolov7@gmail.com>
2022-09-14 13:16:19 +0300

    Add task06-Supermarket

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task06_Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                    continue;
+                }
+                else if (input == "End")
+                {
+                    Console.WriteLine($"{names.Count} people remaining.");
+                    break;
+                }
+                names.Enqueue(input);
+            }
+        }
+    }
+}
