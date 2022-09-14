b5db502e3abc1a9c4e04ec9225d4730ba51f55e3
AleksManolow <amanolov7@gmail.com>
2022-09-15 00:44:47 +0300

    Add task01 Basic Stack Operations

---
+++ C#Advanced/week01_Stacks and Queues/Exercise/task01_Basic Stack Operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task01_Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> numberStack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                numberStack.Push(numbers[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                numberStack.Pop();
+            }
+
+            if (numberStack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numberStack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numberStack.Min());
+            }
+        }
+    }
+}
