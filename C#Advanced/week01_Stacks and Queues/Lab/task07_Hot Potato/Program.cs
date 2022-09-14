d3edac8a924c30ce8d2f9ab26f4edfb6f366d899
AleksManolow <amanolov7@gmail.com>
2022-09-14 14:54:57 +0300

    Add task07_HotPotato

---
+++ C#Advanced/week01_Stacks and Queues/Lab/task07_Hot Potato/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task07_Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPlayers = Console.ReadLine().Split(' ');
+            Queue<string> players = new Queue<string>(inputPlayers);
+            int n = int.Parse(Console.ReadLine());
+            while (players.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+            Console.WriteLine($"Last in {players.Dequeue()}");
+        }
+    }
+}
