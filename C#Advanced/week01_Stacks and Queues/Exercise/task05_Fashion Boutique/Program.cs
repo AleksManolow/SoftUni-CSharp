47fa9e791e5ccb0dcb6888c27b9bc9578c597e4a
AleksManolow <amanolov7@gmail.com>
2022-09-15 13:47:10 +0300

    Add task05_Fashion Boutique

---
+++ C#Advanced/week01_Stacks and Queues/Exercise/task05_Fashion Boutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task05_Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothingValues = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int capacityOfRack = int.Parse(Console.ReadLine());
+
+            Stack<int> clothingValuesStack = new Stack<int>(clothingValues);
+            int countRack = 0;
+            int countCapacity = capacityOfRack;
+            while (clothingValuesStack.Count > 0)
+            {
+                if (clothingValuesStack.Peek() <= countCapacity)
+                {
+                    countCapacity -= clothingValuesStack.Pop();
+                }
+                else
+                {
+                    countCapacity = capacityOfRack;
+                    countRack++;
+                }
+            }
+            if (countCapacity < capacityOfRack)
+            {
+                countRack++;
+            }
+            Console.WriteLine(countRack);
+        }
+    }
+}
