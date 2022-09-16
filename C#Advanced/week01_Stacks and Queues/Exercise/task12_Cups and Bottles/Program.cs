5eb4a53ebcea595fa7b2d4136d7d61e7cf14d924
AleksManolow <amanolov7@gmail.com>
2022-09-16 17:09:13 +0300

    Add task12_Cups and Bottles

---
+++ C#Advanced/week01_Stacks and Queues/Exercise/task12_Cups and Bottles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task12_Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] infoCups = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] infoBottles = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> cups = new Queue<int>(infoCups);
+            Stack<int> bottles = new Stack<int>(infoBottles);
+            
+            
+            int waterWasted = 0;
+            int topCup = 0;
+            while (cups.Any() && bottles.Any())
+            {
+                int topBottles = bottles.Pop();
+                if (topCup == 0)
+                {
+                    topCup = cups.Dequeue();
+                }
+
+                if (topBottles < topCup)
+                {
+                    
+                   topCup -= topBottles;
+                }
+                else
+                {
+                    
+                    waterWasted += topBottles - topCup;
+                    topCup = 0;
+                }
+            }
+
+            if (cups.Count == 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(' ', bottles)}");
+            }
+            else
+            {
+                Console.WriteLine($"Cups: {string.Join(' ', cups)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {waterWasted}");
+        }
+    }
+}
