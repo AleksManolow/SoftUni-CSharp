5afd5adce97e11cd42ae0c1d33b60cf33ba8852f
AleksManolow <amanolov7@gmail.com>
2022-09-18 16:27:14 +0300

    Add task02_Sum Matrix Columns

---
+++ C#Advanced/week02_Multidimensional Arrays/Lab/task02_Sum Matrix Columns/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace task02_Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizes[0], sizes[1]];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] row = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();  
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int sumCol = 0;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    sumCol += matrix[j, i];
+                }
+                Console.WriteLine(sumCol);
+            }
+        }
+    }
+}
