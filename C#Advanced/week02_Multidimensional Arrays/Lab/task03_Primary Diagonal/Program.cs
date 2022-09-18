a91298ec69740891fa81e22ab00be5dfe142eca9
AleksManolow <amanolov7@gmail.com>
2022-09-18 16:40:11 +0300

    Add task03_Primary Diagonal

---
+++ C#Advanced/week02_Multidimensional Arrays/Lab/task03_Primary Diagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace task03_Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] row = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            int sumDiagonal = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        sumDiagonal += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(sumDiagonal);
+        }
+    }
+}
