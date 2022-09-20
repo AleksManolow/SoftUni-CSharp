80daab34e0c6cbcd5adf2d5482c924a5d27c0899
AleksManolow <amanolov7@gmail.com>
2022-09-21 01:10:35 +0300

    Add task06_Record Unique Names

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Lab/task06_Record Unique Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task06_Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
