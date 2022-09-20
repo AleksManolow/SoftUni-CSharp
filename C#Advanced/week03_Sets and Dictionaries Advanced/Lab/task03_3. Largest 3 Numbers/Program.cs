37b89d37e1e5ef2bcfde82649254a0abd2fc90b7
AleksManolow <amanolov7@gmail.com>
2022-09-21 00:20:30 +0300

    Add task03_Largest 2 Numbers

--- C#Advanced/week03_Sets and Dictionaries Advanced/Lab/task03_3. Largest 3 Numbers/Program.cs
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Lab/task03_3. Largest 3 Numbers/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Linq;
 
-namespace task03_3._Largest_3_Numbers
+namespace task03__Largest_3_Numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Array.ForEach(Console.ReadLine().Split(' ').Select(int.Parse).ToArray().OrderByDescending(x => x).Take(3).ToArray(), x => Console.Write(x + " "));
+            Array.ForEach(Console.ReadLine().
+                Split(' ').
+                Select(int.Parse).
+                ToArray().
+                OrderByDescending(x => x).
+                Take(3).
+                ToArray(), 
+                x => Console.Write(x + " "));
         }
     }
 }
