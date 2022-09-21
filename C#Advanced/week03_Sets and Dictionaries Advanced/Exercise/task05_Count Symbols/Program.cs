328d9a985e2ccaa2ba7503ee0aa74bc61b7c0cad
AleksManolow <amanolov7@gmail.com>
2022-09-21 14:53:00 +0300

    Add task05_Count Symbols

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Exercise/task05_Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task05_Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineInput = Console.ReadLine();
+            Dictionary<char, int> symbolsCount = new Dictionary<char, int>();
+            foreach (var symbol in lineInput)
+            {
+                if (!symbolsCount.ContainsKey(symbol))
+                {
+                    symbolsCount.Add(symbol, 0);
+                }
+                symbolsCount[symbol]++;
+            }
+            foreach (var symbol in symbolsCount.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
