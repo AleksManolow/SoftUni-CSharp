485d5e96ec2524bd4c683aa12dc6d02ce1771bf2
AleksManolow <amanolov7@gmail.com>
2022-09-21 14:31:25 +0300

    Add task04_Even Times

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Exercise/task04_Even Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task04_Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbersCount = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!numbersCount.ContainsKey(number))
+                {
+                    numbersCount.Add(number, 0);
+                }
+                numbersCount[number]++;
+            }
+            numbersCount = numbersCount.Where(x => x.Value % 2 == 0).ToDictionary(x => x.Key, x => x.Value);
+            foreach (int number in numbersCount.Keys)
+                Console.WriteLine(number);
+
+        }
+    }
+}
