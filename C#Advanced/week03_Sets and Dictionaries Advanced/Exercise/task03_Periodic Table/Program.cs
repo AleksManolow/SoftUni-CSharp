55915ef169f4aebd70b6afd6f1523b425994bf04
AleksManolow <amanolov7@gmail.com>
2022-09-21 12:29:22 +0300

    Add task03_Periodic Table

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Exercise/task03_Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task03_Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> chemicalCompounds = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                foreach (var c in input)
+                {
+                        chemicalCompounds.Add(c);
+                }
+            }
+            chemicalCompounds = chemicalCompounds.OrderBy(x => x).ToHashSet();
+            foreach (var c in chemicalCompounds)
+            {
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
