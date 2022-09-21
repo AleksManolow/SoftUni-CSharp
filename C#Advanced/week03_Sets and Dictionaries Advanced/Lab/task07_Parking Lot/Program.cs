02e750d6f57aa75f20dd9d4e38587efdd7fad2ca
AleksManolow <amanolov7@gmail.com>
2022-09-21 11:31:24 +0300

    Add task07_Parking Lot

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Lab/task07_Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task07_Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            HashSet<string> carsNumbers = new HashSet<string>();
+            while (input[0] != "END")
+            {
+                if (input[0] == "IN")
+                {
+                    carsNumbers.Add(input[1]);
+                }
+                else if (input[0] == "OUT")
+                {
+                    carsNumbers.Remove(input[1]);
+                }
+
+
+                input = Console.ReadLine().Split(", ");
+            }
+            if (carsNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var carNumber in carsNumbers)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+
+        }
+    }
+}
