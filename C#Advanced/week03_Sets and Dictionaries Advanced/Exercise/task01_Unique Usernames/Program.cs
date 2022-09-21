f80306f1e62884e9fc774bd279bec9ffc81f2bb6
AleksManolow <amanolov7@gmail.com>
2022-09-21 12:06:29 +0300

    Add task01_Unique Usernames

---
+++ C#Advanced/week03_Sets and Dictionaries Advanced/Exercise/task01_Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> collection = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                collection.Add(Console.ReadLine());
+            }
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
