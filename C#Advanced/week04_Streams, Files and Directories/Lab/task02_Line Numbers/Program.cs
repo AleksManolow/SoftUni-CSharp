e05c11d507866442171f96ea4f4ba3707a91e676
AleksManolow <amanolov7@gmail.com>
2022-09-23 12:44:57 +0300

    Add task02_Line Number

---
+++ C#Advanced/week04_Streams, Files and Directories/Lab/task02_Line Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader read = new StreamReader(inputFilePath))
+            {
+                int count = 0;
+                string line = read.ReadLine();
+                using (StreamWriter write = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        write.WriteLine(count + ". " + line);
+                        count++;
+                        line = read.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
