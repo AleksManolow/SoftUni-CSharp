98aa53a5b3c2a8142317b3e6ac46c507c74799b2
AleksManolow <amanolov7@gmail.com>
2022-09-23 15:02:58 +0300

    Add task04_Merge Files

---
+++ C#Advanced/week04_Streams, Files and Directories/Lab/task04_Merge Text Files/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using (StreamReader readFirstFile = new StreamReader(firstInputFilePath))
+            {
+                using (StreamReader readSecoundFile = new StreamReader(secondInputFilePath))
+                {
+                    string lineF = readFirstFile.ReadLine();
+                    string lineS = readSecoundFile.ReadLine();
+                    using (StreamWriter write = new StreamWriter(outputFilePath))
+                    {
+                        while (lineF != null || lineS != null)
+                        {
+                            if (lineF != null)
+                            {
+                                write.WriteLine(lineF);
+                            }
+
+                            if (lineS != null)
+                            {
+                                write.WriteLine(lineS);
+                            }
+                            lineF = readFirstFile.ReadLine();
+                            lineS = readSecoundFile.ReadLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
