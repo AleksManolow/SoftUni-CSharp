efedb90b28f20aaeccd087c6173f13bb9c890f98
AleksManolow <amanolov7@gmail.com>
2022-09-24 01:08:37 +0300

    Add task03_Copy File

---
+++ C#Advanced/week04_Streams, Files and Directories/Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,34 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream fs1 = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream fs2 = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        int bytesRead = fs1.Read(buffer);
+                        if (bytesRead == 0) break;
+                        fs2.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
