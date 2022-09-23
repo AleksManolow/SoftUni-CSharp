fa1a25b083457e5d38fa7787759a3a3720b23b67
AleksManolow <amanolov7@gmail.com>
2022-09-23 12:28:08 +0300

    Add task01_Odd Lines

---
+++ C#Advanced/week04_Streams, Files and Directories/Lab/task01_OddLines/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader read = new StreamReader(inputFilePath))
+            {
+                int count = 0;
+                string line = read.ReadLine();  
+                using (StreamWriter write = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (count % 2 == 1)
+                            write.WriteLine(line);
+                        count++;
+                        line = read.ReadLine();
+                    }  
+                }
+            }
+        }
+    }
+}
+
