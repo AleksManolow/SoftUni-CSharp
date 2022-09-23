575a82d32c562fa5dad2cc549353e1eed0660b9d
AleksManolow <amanolov7@gmail.com>
2022-09-23 16:35:39 +0300

    Add task01 changes

--- C#Advanced/week04_Streams, Files and Directories/Exercise/EvenLines/EvenLines.cs
+++ C#Advanced/week04_Streams, Files and Directories/Exercise/EvenLines/EvenLines.cs
@@ -1,6 +1,8 @@
 ﻿namespace EvenLines
 {
     using System;
+    using System.IO;
+
     public class EvenLines
     {
         static void Main()
@@ -12,7 +14,32 @@
 
         public static string ProcessLines(string inputFilePath)
         {
-            throw new NotImplementedException();
+            string outStr = string.Empty;
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int count = 0;
+                while (line != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        line = string.Join('@', line.Split(new char[] { '-', ',', '.', '!', '?' }));
+                        string[] tempArr = line.Split(' ');
+                        for (int i = tempArr.Length - 1; i >= 0; i--)
+                        {
+                            outStr = outStr + ' ' + tempArr[i];
+                        }
+                        outStr += "\n";    
+                    }
+                    
+                    count++;
+                    line = reader.ReadLine();
+                }
+            }
+
+
+            return outStr;
         }
     }
 }
