5b6b124b0dd316f46852a5a617f43ce39d8fc827
AleksManolow <amanolov7@gmail.com>
2022-09-23 17:07:48 +0300

    Add task02_Line Number

---
+++ C#Advanced/week04_Streams, Files and Directories/Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,55 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Reflection.PortableExecutable;
+    using System.Threading;
+    using static System.Net.Mime.MediaTypeNames;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string line = reader.ReadLine();
+                    int countLine = 0;
+
+                    while (line != null)
+                    {
+                        int characters = 0;
+                        int punctuationMarks = 0;
+
+                        foreach (var ch in line)
+                        {
+                            if (ch == '-' || ch == '.' || ch == ',' || ch == '!'
+                                || ch == '?' || ch == '\'')
+                            {
+                                punctuationMarks++;
+                            }
+                            else if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+                            {
+                                characters++;
+                            }
+                        }
+                       
+                        writer.WriteLine($"Line {countLine}: {line} ({characters})({punctuationMarks})");
+
+                        countLine++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
