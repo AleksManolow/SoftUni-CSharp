c7ac63865d578616cc94e98db0bb032ddf40ce04
AleksManolow <amanolov7@gmail.com>
2022-09-27 00:15:53 +0300

    Add part of the tasks from Defining Classes - Lab

---
+++ C#Advanced/week06_Defining Classes/Lab/task03_Car Constructors/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using _03._Car_Constructors;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car fisrtCar = new Car();
+            Car secoundCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+        }
+    }
+}
---
+++ C#Advanced/week06_Defining Classes/Lab/task04_Car Engine And Tires/Tire.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    internal class Tire
+    {
+		private int year;
+		private double pressure;
+		public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+}
