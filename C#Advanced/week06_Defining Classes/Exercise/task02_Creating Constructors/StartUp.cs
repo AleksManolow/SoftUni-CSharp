f0c1863b53656165a5ce0546151118850a2719a8
AleksManolow <amanolov7@gmail.com>
2022-09-27 15:43:51 +0300

    Add task01 and task02 Difining Calsses

---
+++ C#Advanced/week06_Defining Classes/Exercise/task02_Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Aleks", 21);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
