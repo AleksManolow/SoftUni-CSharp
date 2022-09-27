9d399be93736b20178c6808a8824f605db431e9a
AleksManolow <amanolov7@gmail.com>
2022-09-28 00:04:22 +0300

    Add task05_Date Modifier

---
+++ C#Advanced/week06_Defining Classes/Exercise/task05_Date Modifier/DateModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static int CalculateDifference(string firstDate, string secoundDate)
+        {
+            DateTime first = DateTime.Parse(firstDate);
+            DateTime secound = DateTime.Parse(secoundDate);
+
+            TimeSpan difference = first - secound;
+
+
+
+            return Math.Abs(difference.Days); 
+        }
+    }
+}
---
+++ C#Advanced/week06_Defining Classes/Exercise/task05_Date Modifier/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.CalculateDifference(firstDate, secondDate));
+        }
+    }
+}
