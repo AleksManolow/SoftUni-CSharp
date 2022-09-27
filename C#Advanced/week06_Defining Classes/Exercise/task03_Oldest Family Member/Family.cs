e4d8bfc3a3929f5586f9896493b01653a49ec465
AleksManolow <amanolov7@gmail.com>
2022-09-27 23:33:21 +0300

    Add task03_Oldest Family Member

---
+++ C#Advanced/week06_Defining Classes/Exercise/task03_Oldest Family Member/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        List<Person> people;
+        public Family()
+        {
+            people = new List<Person>();
+        }
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            return people.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+    }
+}
---
+++ C#Advanced/week06_Defining Classes/Exercise/task03_Oldest Family Member/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                Person person = new Person(input[0], int.Parse(input[1]));
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
