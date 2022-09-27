bd58398a0f839421b9d40f4c64b18193082e5bfb
AleksManolow <amanolov7@gmail.com>
2022-09-27 23:42:18 +0300

    Add task04_Opinion Poll

---
+++ C#Advanced/week06_Defining Classes/Exercise/task04_Opinion Poll/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+        public Person(int age): this()
+        {
+            Age = age;
+        }
+        public Person(string name, int age):this(age)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ C#Advanced/week06_Defining Classes/Exercise/task04_Opinion Poll/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                people.Add(new Person(input[0], int.Parse(input[1])));
+            }
+            foreach (var person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine(person.Name + " - " + person.Age);
+            }
+        }
+    }
+}
