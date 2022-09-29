3501f81e7847ab7d287ffc9b2b6d9589d153373a
AleksManolow <amanolov7@gmail.com>
2022-09-29 14:50:16 +0300

    Add task10_SoftUni Parking

---
+++ C#Advanced/week06_Defining Classes/Exercise/task10_SoftUni Parking/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+		private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        public string Make
+		{
+			get { return make; }
+			set { make = value; }
+		}
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		public int  HorsePower
+		{
+			get { return horsePower; }
+			set { horsePower = value; }
+		}
+		public string RegistrationNumber
+		{
+			get { return registrationNumber; }
+			set { registrationNumber = value; }
+		}
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"Make: {this.Make}");
+            output.AppendLine($"Model: {this.Model}");
+            output.AppendLine($"HorsePower: {this.HorsePower}");
+            output.Append($"RegistrationNumber: {this.RegistrationNumber}");
+            return output.ToString();
+        }
+
+
+
+
+    }
+}
