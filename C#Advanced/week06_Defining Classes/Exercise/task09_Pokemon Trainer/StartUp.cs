0192a10a0af8f1f5d06510de6ca164203de1eb90
AleksManolow <amanolov7@gmail.com>
2022-09-29 13:17:34 +0300

    Add task09_Pokemon Trainer

---
+++ C#Advanced/week06_Defining Classes/Exercise/task09_Pokemon Trainer/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] inputTrainers = Console.ReadLine().Split(' ');
+            List<Trainer> trainers = new List<Trainer>();
+            while (inputTrainers[0] != "Tournament")
+            {
+
+                if (trainers.SingleOrDefault(x => x.Name == inputTrainers[0]) == null)
+                {
+                    trainers.Add(new Trainer(inputTrainers[0]));
+                }
+                trainers
+                    .Single(x => x.Name == inputTrainers[0])
+                    .Pokemons
+                    .Add(new Pokemon(inputTrainers[1], inputTrainers[2], int.Parse(inputTrainers[3])));
+                inputTrainers = Console.ReadLine().Split(' ');
+            }
+
+            string inputElements = Console.ReadLine();
+            while (inputElements != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Where(x => x.Element == inputElements).Count() == 0)
+                    {
+                        
+                        for (int i = 0; i < trainer.Pokemons.Count; i++)
+                        {
+                            trainer.Pokemons[i].Health -= 10;
+                        }
+                        trainer.Pokemons.RemoveAll(x => x.Health <= 0);
+                    }
+                    else
+                    {
+                        trainer.NumberOfBadges += 1;
+                    }
+                }
+                inputElements = Console.ReadLine();
+            }
+            trainers = trainers.OrderByDescending(x => x.NumberOfBadges).ToList();
+
+            foreach (var trainer in trainers)
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ C#Advanced/week06_Defining Classes/Exercise/task09_Pokemon Trainer/Trainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> pokemons;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int NumberOfBadges
+        {
+            get { return numberOfBadges; }
+            set { numberOfBadges = value; }
+        }
+        public List<Pokemon> Pokemons
+        {
+            get { return pokemons; }
+            set { pokemons = value; }
+        }
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+            Pokemons = new List<Pokemon>(); 
+        }
+
+    }
+}
