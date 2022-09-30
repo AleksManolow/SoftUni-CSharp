84e264ceb4c4536924b48ae9bf250fc53dace61a
AleksManolow <amanolov7@gmail.com>
2022-09-30 19:15:40 +0300

    Add Generics - Lab

---
+++ C#Advanced/week07_Generics/Lab/task01_Box of T/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> box;
+
+        public Box()
+        {
+            box = new List<T>();
+        }
+
+        public void Add (T element)
+        {
+            box.Insert(0, element);
+        }
+
+        public T Remove()
+        {
+            T removedEment = box[0];
+            box.RemoveAt(0);
+            return removedEment;
+        }
+
+        public int Count => box.Count;
+
+    }
+}
---
+++ C#Advanced/week07_Generics/Lab/task03_Generic Scale/StartUp .cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //EqualityScale<int> equalityScale =new  EqualityScale<int>(5, 12);
+            //Console.WriteLine(equalityScale.AreEqual());
+
+        }
+    }
+}
