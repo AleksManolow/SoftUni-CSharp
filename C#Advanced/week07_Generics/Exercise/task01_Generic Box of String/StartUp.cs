10f9b2b5a9761a0061ca232514d2c3051ff0e6e4
AleksManolow <amanolov7@gmail.com>
2022-10-01 11:37:32 +0300

    Add task01_Generic Box of String

---
+++ C#Advanced/week07_Generics/Exercise/task01_Generic Box of String/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01_Generic_Box_of_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> list = new List<Box<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                list.Add(new Box<string>(input));
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
