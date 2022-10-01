7e3666bbcae0797bdfecd4d0297f5d00bf257120
AleksManolow <amanolov7@gmail.com>
2022-10-01 16:20:11 +0300

    Add task05_Generic Count Method String

---
+++ C#Advanced/week07_Generics/Exercise/task05_Generic Count Method String/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task05_Generic_Count_Method_String
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string item = Console.ReadLine();
+                boxes.Add(new Box<string>(item));
+            }
+            string itemToCompare = Console.ReadLine();
+            Console.WriteLine(CompareItems(boxes, itemToCompare));
+        }
+        public static int CompareItems<T>(List<Box<T>> boxes, T item)
+            where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (var box in boxes)
+            {
+                if (box.CompareTo(item) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    
+    }
+}
