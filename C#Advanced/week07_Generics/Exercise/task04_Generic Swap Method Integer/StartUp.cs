ee01eed03d1d74cc95cc8becb5bcbc50078b45f4
AleksManolow <amanolov7@gmail.com>
2022-10-01 16:13:24 +0300

    Add task04_Generic Swap Method Integer

---
+++ C#Advanced/week07_Generics/Exercise/task04_Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task04_Generic_Swap_Method_Integer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<int>(list);
+            int[] indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
