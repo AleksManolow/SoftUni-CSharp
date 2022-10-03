4bea241124efbf90c394d4de2fae16526d2b67de
AleksManolow <amanolov7@gmail.com>
2022-10-03 16:28:05 +0300

    Add task02_Collection

---
+++ C#Advanced/week08_Iterators and Comparators/Exercise/task02_Collection/ListyIterator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int index;
+
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext() 
+            => index + 1 < elements.Count;  
+
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new Exception("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+                => this.GetEnumerator();
+    }
+}
