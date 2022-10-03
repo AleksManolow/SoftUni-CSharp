6743e564c5373d2f13874b0e150da93971db569e
AleksManolow <amanolov7@gmail.com>
2022-10-03 19:03:38 +0300

    Add task04_Froggy

---
+++ C#Advanced/week08_Iterators and Comparators/Exercise/task04_Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04_Froggy
+{
+    public class Lake:IEnumerable<int>
+    {
+        private List<int> elements;
+
+        public Lake(List<int> elements)
+        {
+            this.elements = elements;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return elements[i];
+                }
+            }
+
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return elements[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();
+    }
+}
---
+++ C#Advanced/week08_Iterators and Comparators/Exercise/task04_Froggy/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task04_Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> elements = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            Lake lake = new Lake(elements);
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
