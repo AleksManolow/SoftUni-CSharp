94e9c623ed41ea24d87332ce4bcb4d2a537e167c
AleksManolow <amanolov7@gmail.com>
2022-10-04 15:54:32 +0300

    Add task07_Custom Comparator

---
+++ C#Advanced/week08_Iterators and Comparators/Exercise/task07_Custom Comparator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+
+namespace task07_Custom_Comparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(array);
+            var sortedArray = array.OrderBy(number => number % 2 != 0).ThenBy(number => number % 2 == 0);
+            Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
