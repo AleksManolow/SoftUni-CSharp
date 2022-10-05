d9b308987a5a8f3b845db0d76a0a06c4c185f3c1
AleksManolow <amanolov7@gmail.com>
2022-10-05 15:19:20 +0300

    Add Basic Algorithms - Exercise

---
+++ C#Advanced/week09_Algorithms Introduction/Exercise/task06_Bubble Sort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task06_Bubble_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 20, 42, 14, 37, 4, 33, 50, 10, 36, 19, 40, 13, 24, 34, 49 };
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i + 1 < array.Length && array[i] > array[i + 1])
+                    {
+                        isSorted = false;
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
