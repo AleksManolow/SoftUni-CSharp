75381da46a33c6539347b54a01393eac5986e058
AleksManolow <amanolov7@gmail.com>
2022-10-05 14:44:59 +0300

    Add task02_Recursive Factorial

---
+++ C#Advanced/week09_Algorithms Introduction/Exercise/task02_Recursive Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task02_Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        private static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
