5176323798a80dc964c1a44fe01e58e63bbfc320
AleksManolow <amanolov7@gmail.com>
2022-10-05 14:41:44 +0300

    Add task01_Recursive Array Sum

---
+++ C#Advanced/week09_Algorithms Introduction/Exercise/task01_Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace task01_Recursive_Array_Sum
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(arr, 0));
+        }
+
+        public static int Sum(int[] arr, int index)
+        {
+            if (index == arr.Count() - 1)
+            {
+                return arr[index];
+            }
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
