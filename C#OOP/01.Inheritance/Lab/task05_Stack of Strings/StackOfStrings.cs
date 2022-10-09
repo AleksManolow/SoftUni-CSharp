a675db687832b9874f1756daf6498b8f2827b1e6
AleksManolow <amanolov7@gmail.com>
2022-10-09 19:44:39 +0300

    Add task05_Stack of Strings

---
+++ C#OOP/01.Inheritance/Lab/task05_Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings:Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> items)
+        {
+
+            foreach (var item in items)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+
+    }
+}
