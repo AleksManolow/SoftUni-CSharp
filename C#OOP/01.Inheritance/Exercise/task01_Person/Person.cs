0cd6ad399329d7b3aed696e5d2dcaafd699597d9
AleksManolow <amanolov7@gmail.com>
2022-10-10 23:31:31 +0300

    Add task01_Person

---
+++ C#OOP/01.Inheritance/Exercise/task01_Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;
+
+        public virtual int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 )
+                {
+                    throw new Exception();
+                }
+                age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
