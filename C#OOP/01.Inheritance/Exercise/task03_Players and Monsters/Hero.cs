44926f66a67231d33b3a62f9675d1e0b97ebf337
AleksManolow <amanolov7@gmail.com>
2022-10-11 14:14:07 +0300

    Add task03_Players and Monsters

---
+++ C#OOP/01.Inheritance/Exercise/task03_Players and Monsters/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        public Hero(string name, int level)
+        {
+            this.Name = name;
+            this.Level = level;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Name} Level: {this.Level}";
+        }
+
+    }
+}
---
+++ C#OOP/01.Inheritance/Exercise/task03_Players and Monsters/Wizard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Wizard:Hero
+    {
+        public Wizard(string name, int level)
+            :base(name, level)
+        {
+
+        }
+        
+    }
+}
