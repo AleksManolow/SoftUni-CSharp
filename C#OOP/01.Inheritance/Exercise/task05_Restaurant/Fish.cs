c7b5bf75fc8235a41dd6a5218b6962400d0808b3
AleksManolow <amanolov7@gmail.com>
2022-10-13 01:21:32 +0300

    Add task05_Restaurant

---
+++ C#OOP/01.Inheritance/Exercise/task05_Restaurant/Fish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Fish:MainDish
+    {
+        public Fish(string name, decimal price)
+            : base(name, price, 22)  
+        {
+
+        }
+    }
+}
---
+++ C#OOP/01.Inheritance/Exercise/task05_Restaurant/StarUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
