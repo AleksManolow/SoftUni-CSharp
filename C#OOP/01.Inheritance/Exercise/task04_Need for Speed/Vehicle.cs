71fade3a8972730526c2c509bbc42af3c78abc33
AleksManolow <amanolov7@gmail.com>
2022-10-12 13:25:42 +0300

    Add task04_Need For Speed

---
+++ C#OOP/01.Inheritance/Exercise/task04_Need for Speed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
