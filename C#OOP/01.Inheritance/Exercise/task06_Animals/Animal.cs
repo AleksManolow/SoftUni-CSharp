fdf36e7fb3a050912c7b3512397e2f4765f31245
AleksManolow <amanolov7@gmail.com>
2022-10-14 16:54:54 +0300

    Add task06_Animals

---
+++ C#OOP/01.Inheritance/Exercise/task06_Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        public virtual string ProduceSound() => "";
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Gender}";
+        }
+
+    }
+}
---
+++ C#OOP/01.Inheritance/Exercise/task06_Animals/Kitten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Kitten:Cat
+    {
+        public Kitten(string name, int age)
+            : base(name, age, "female")
+        {
+
+        }
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
