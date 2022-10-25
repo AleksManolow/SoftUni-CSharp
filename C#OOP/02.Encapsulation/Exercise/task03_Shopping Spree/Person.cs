588971d2e8ceb0cd7d5cf286cdf6b9ed24a2b24b
AleksManolow <amanolov7@gmail.com>
2022-10-25 14:59:09 +0300

    Add task03_Shopping Spree

---
+++ C#OOP/02.Encapsulation/Exercise/task03_Shopping Spree/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products => products;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public void AddProductsToBag(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.products.Add(product);
+                this.Money -= product.Cost;
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Products.Count > 0
+                ? $"{this.Name} - {string.Join(", ", products.Select(p => p.Name))}"
+                : $"{this.Name} - Nothing bought";
+        }
+    }
+}
