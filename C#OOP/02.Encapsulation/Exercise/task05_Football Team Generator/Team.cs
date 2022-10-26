cc30683bf9aba2e7cb3d8c3e66d4e16965c64c91
AleksManolow <amanolov7@gmail.com>
2022-10-27 00:00:37 +0300

    Add task05_Football Team Generator

---
+++ C#OOP/02.Encapsulation/Exercise/task05_Football Team Generator/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Team
+    {
+		private List<Player> players;
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public Team(string name)
+		{
+			this.Name = name;
+            this.players = new List<Player>();
+        }
+
+		public double Rating => players.Count == 0
+                ? 0
+                : Math.Round(players.Average(p => p.SkillLevel));
+
+		public void AddPlayer(Player player)
+		{
+			players.Add(player);
+		}
+
+		public void RemovePlayer(string playerName)
+		{
+			Player player = players.FirstOrDefault(p => p.Name == playerName);
+
+			if (player == null)
+			{
+				throw new ArgumentException($"Player {playerName} is not in {this.Name} team.");
+			}
+			players.Remove(player);
+		}
+
+    }
+}
