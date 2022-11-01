4ddc155b06286722e44b60c9e850a3d172c16aab
AleksManolow <amanolov7@gmail.com>
2022-11-02 00:15:33 +0200

    Add task02_Animals

---
+++ C#OOP/04.Polymorphism/Lab/task02_Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string FavouriteFood { get; set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;  
+        }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouriteFood}";
+        }
+    }
+}
