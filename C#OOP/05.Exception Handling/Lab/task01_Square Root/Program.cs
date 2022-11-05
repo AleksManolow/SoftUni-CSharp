d37cc0894f7a4957f874dd36af6817542fcc50f2
AleksManolow <amanolov7@gmail.com>
2022-11-05 19:18:50 +0200

    Add task01_Square Root

---
+++ C#OOP/05.Exception Handling/Lab/task01_Square Root/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task01_Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                double result = Math.Sqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
