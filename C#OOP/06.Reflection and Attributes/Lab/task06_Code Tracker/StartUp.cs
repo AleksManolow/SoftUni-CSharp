919071f8071e54afe73d9b3a9556c56d0549b352
AleksManolow <amanolov7@gmail.com>
2022-11-10 12:35:32 +0200

    Add task06_Create Attribute

---
+++ C#OOP/06.Reflection and Attributes/Lab/task06_Code Tracker/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    public class StartUp
+    {
+        [Author("Marina")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+
+        [Author("Vanya")]
+        public void BlahBlah()
+        {
+
+        }
+        [Author("Ivalina")]
+        public static void Something()
+        {
+
+        }
+    }
+}
---
+++ C#OOP/06.Reflection and Attributes/Lab/task06_Code Tracker/Tracker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public Tracker()
+        {
+
+        }
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(t => t.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute authorAttribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
