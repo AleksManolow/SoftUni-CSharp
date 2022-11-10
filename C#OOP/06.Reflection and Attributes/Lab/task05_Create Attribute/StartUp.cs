c789611e02da3f0f66fe4511e77e16c56d31cca9
AleksManolow <amanolov7@gmail.com>
2022-11-10 12:31:23 +0200

    Add task05_Create Attribute

---
+++ C#OOP/06.Reflection and Attributes/Lab/task05_Create Attribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Aleks")]
+    public class StartUp
+    {
+        [Author("Hice")]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
