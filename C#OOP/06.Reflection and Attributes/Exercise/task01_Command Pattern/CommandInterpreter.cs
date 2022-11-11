2647c432f659d5b3e7ff0b9561fa11398081c973
AleksManolow <amanolov7@gmail.com>
2022-11-11 12:18:34 +0200

    Add task01_Command Pattern

---
+++ C#OOP/06.Reflection and Attributes/Exercise/task01_Command Pattern/CommandInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] input = args.Split(' ');
+
+            string commandName = input[0];
+            string[] value = input.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == commandName + "Command");
+            if (type == null)
+            {
+                throw new InvalidOperationException("Missing command");
+            }
+
+            Type commandInterface = type.GetInterface("ICommand");
+            if (commandInterface == null)
+            {
+                throw new InvalidOperationException("Not a command");
+            }
+
+            var command = Activator.CreateInstance(type) as ICommand;
+            string result = command.Execute(value);
+
+            return result;
+        }
+    }
+}
---
+++ C#OOP/06.Reflection and Attributes/Exercise/task01_Command Pattern/Engine.cs
@@ -0,0 +1,23 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string args = Console.ReadLine();
+                string result = commandInterpreter.Read(args);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
