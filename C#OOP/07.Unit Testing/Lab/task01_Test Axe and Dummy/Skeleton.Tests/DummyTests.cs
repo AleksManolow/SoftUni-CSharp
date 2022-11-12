852b9c63acbc42ad06a628b5f30858ffbdb05cf7
AleksManolow <amanolov7@gmail.com>
2022-11-12 18:39:21 +0200

    Add Lab Unit Testing

---
+++ C#OOP/07.Unit Testing/Lab/task01_Test Axe and Dummy/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyShouldLooseHealthWhenAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            dummy.TakeAttack(5);
+    
+            Assert.AreEqual(5, dummy.Health);
+        }
+        [Test]
+        public void DeadDummyShouldThrowExceptionWhenAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(5));
+        }
+        [Test]
+        public void DeadDummyShouldGiveXp()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.AreEqual(10, dummy.GiveExperience());
+        }
+        [Test]
+        public void AliveDummyShouldNotGiveXp()
+        {
+            Dummy dummy = new Dummy(10, 10);
+
+            Assert.That(() => dummy.GiveExperience(), Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+
+        }
+    }
+}
