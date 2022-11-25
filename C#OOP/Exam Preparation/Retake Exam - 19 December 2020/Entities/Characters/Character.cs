a050fd61967f84106db59454bc39429b12fa93d2
AleksManolow <amanolov7@gmail.com>
2022-11-25 16:21:54 +0200

    Add task01 from Retake Exam - 19 December 2020 and Skeleton on this Exam

---
+++ C#OOP/Exam Preparation/Retake Exam - 19 December 2020/Entities/Characters/Character.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+using WarCroft.Constants;
+using WarCroft.Entities.Inventory;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Characters.Contracts
+{
+    public abstract class Character
+    {
+		private string name;
+
+		private double health;
+
+		private double armor;
+
+		public Character(string name, double health, double armor, double abilityPoints, Bag bag)
+		{
+			Name= name;
+			BaseHealth= health;
+			Health= health;
+			BaseArmor = armor;
+			Armor= armor;	
+			AbilityPoints= abilityPoints;
+			Bag = bag;	
+		}
+		
+		public IBag Bag { get; private set; }
+		public double AbilityPoints { get; private set; }
+		public double Armor
+		{
+			get 
+			{ 
+				return armor;
+			}
+			private set 
+			{ 
+				armor = value;
+				if (armor < 0)
+				{
+					armor = 0;
+				}
+			}
+		}
+
+		public double BaseArmor  { get; private set; }
+		public double Health
+        {
+			get 
+			{ 
+				return health;
+			}
+			set 
+			{
+				health = value;
+                if (health < 0)
+                {
+                    health = 0;
+					IsAlive= true;
+                }
+
+                if (health > BaseHealth)
+				{
+					health = BaseHealth;
+				}
+				
+				
+			}
+		}
+
+		public double BaseHealth { get; private set; }
+		public string Name
+		{
+			get 
+			{ 
+				return name;
+			}
+			private set 
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException(ExceptionMessages.CharacterNameInvalid);
+				}
+				name = value;
+			}
+		}
+
+		public bool IsAlive { get; set; } = true;
+
+		protected void EnsureAlive()
+		{
+			if (!this.IsAlive)
+			{
+				throw new InvalidOperationException(ExceptionMessages.AffectedCharacterDead);
+			}
+		}
+
+        public void TakeDamage(double hitPoints)
+		{
+			this.EnsureAlive();
+			double leftPoints = hitPoints - this.Armor > 0 ? hitPoints - this.Armor : 0;
+			this.Armor -= hitPoints;
+			this.Health -= leftPoints;
+
+			if (this.Health == 0)
+			{
+				IsAlive= false;
+			}
+		}
+        public void UseItem(Item item)
+		{
+            this.EnsureAlive();
+			item.AffectCharacter(this);
+        }
+
+    }
+}
---
+++ C#OOP/Exam Preparation/Retake Exam - 19 December 2020/Entities/Inventory/Bag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WarCroft.Constants;
+using WarCroft.Entities.Items;
+
+namespace WarCroft.Entities.Inventory
+{
+    public abstract class Bag : IBag
+    {   
+        private readonly List<Item> items;
+        public Bag(int capacity = 100)
+        {
+            Capacity= capacity;
+            items = new List<Item>();
+
+        }
+        public int Capacity { get; set; }
+
+        public int Load => Items.Sum(w => w.Weight);
+
+        public IReadOnlyCollection<Item> Items 
+            => items;
+
+        public void AddItem(Item item)
+        {
+            if(this.Load + item.Weight > this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.ExceedMaximumBagCapacity);
+            }
+            this.items.Add(item);
+        }
+        public Item GetItem(string name)
+        {
+            if (this.items.Count == 0)
+            {
+                throw new InvalidOperationException(ExceptionMessages.EmptyBag);
+            }
+            Item item = items.FirstOrDefault(x => x.GetType().Name == name);
+            if (item == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.ItemNotFoundInBag, name));
+            }
+            this.items.Remove(item);
+            return item;
+        }
+    }
+}
