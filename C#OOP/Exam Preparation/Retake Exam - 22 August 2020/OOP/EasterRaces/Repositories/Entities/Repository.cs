0c2063514fe2b816be34449810c19f7edc4f2ffe
AleksManolow <amanolov7@gmail.com>
2022-12-06 11:37:34 +0200

    Add task01 and task02 from Retake Exam - 22 August 2020

---
+++ C#OOP/Exam Preparation/Retake Exam - 22 August 2020/OOP/EasterRaces/Repositories/Entities/Repository.cs
@@ -0,0 +1,35 @@
+﻿using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        private List<T> models;
+
+        protected Repository()
+        {
+            models = new List<T>();
+        }
+        public List<T> Models => models;
+
+        public abstract T GetByName(string name);
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            return models as IReadOnlyCollection<T>;
+        }
+
+        public void Add(T model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(T model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
