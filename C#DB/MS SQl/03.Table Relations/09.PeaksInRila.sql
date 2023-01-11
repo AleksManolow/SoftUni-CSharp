fad2085a4548d0e3d6fba37873672c796443fecd
AleksManolow <amanolov7@gmail.com>
2023-01-11 20:24:06 +0200

    Add Query09-Peaks in Rila

---
+++ C#DB/MS SQl/03.Table Relations/09.PeaksInRila.sql
@@ -0,0 +1,8 @@
+SELECT 
+	[Mountains].MountainRange,
+	[Peaks].PeakName, 
+	[Peaks].Elevation
+FROM [Mountains]
+JOIN [Peaks] ON [Mountains].Id = [Peaks].[MountainId]
+WHERE [Mountains].[MountainRange] = 'Rila'
+ORDER BY [Peaks].Elevation DESC
