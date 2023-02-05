5ef7aa1a915cff8ca94a06a6f0c175aacfdc9b9c
AleksManolow <amanolov7@gmail.com>
2023-02-05 16:57:48 +0200

    Add  07. Top 20 Flight Destinations

---
+++ C#DB/MS SQl/Exam Preparation/Retake Exam - 10 Dec 2021/07.Top20FlightDestinations.sql
@@ -0,0 +1,11 @@
+SELECT TOP(20)
+	fd.Id AS [DestinationId],
+	fd.Start,
+	p.FullName,
+	a.AirportName,
+	fd.TicketPrice
+FROM FlightDestinations AS fd
+JOIN Passengers AS p ON p.Id = fd.PassengerId 
+JOIN Airports AS a ON a.Id = fd.AirportId
+WHERE DATEPART(DAY, Start) % 2 = 0
+ORDER BY fd.TicketPrice DESC, a.AirportName
