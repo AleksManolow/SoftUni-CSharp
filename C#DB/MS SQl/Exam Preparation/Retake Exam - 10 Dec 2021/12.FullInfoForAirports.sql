e3b6cb15a80f6b1815606887985887e3b8aee30f
AleksManolow <amanolov7@gmail.com>
2023-02-07 12:05:32 +0200

    Add 12. Full Info for Airports

---
+++ C#DB/MS SQl/Exam Preparation/Retake Exam - 10 Dec 2021/12.FullInfoForAirports.sql
@@ -0,0 +1,24 @@
+CREATE OR ALTER PROC usp_SearchByAirportName @airportName VARCHAR(70)
+AS
+SELECT 
+	a.AirportName,
+	p.FullName,
+	CASE
+		WHEN fd.TicketPrice < 400 THEN 'Low'
+		WHEN fd.TicketPrice BETWEEN 401 AND 1500 THEN 'Medium'
+		WHEN fd.TicketPrice > 1500 THEN 'High'
+	END		AS LevelOfTicketPrice,
+	ac.Manufacturer,
+	ac.Condition,
+	at.TypeName
+FROM Airports AS a
+JOIN FlightDestinations AS fd ON fd.AirportId = a.Id
+JOIN Passengers AS p ON p.Id = fd.PassengerId
+JOIN Aircraft AS ac ON ac.Id = fd.AircraftId
+JOIN AircraftTypes AS at ON at.Id = ac.TypeId
+WHERE AirportName = 'Sir Seretse Khama International Airport'
+ORDER BY ac.Manufacturer, p.FullName
+
+GO 
+
+EXEC usp_SearchByAirportName 'Sir Seretse Khama International Airport'
