26891d260df23b328388b8341568414d1a9941b1
AleksManolow <amanolov7@gmail.com>
2023-02-17 11:11:00 +0200

    Add 05. Mechanic Assignments

---
+++ C#DB/MS SQl/Exam Preparation/Exam - 27 Jun 2020/05.MechanicAssignments.sql
@@ -0,0 +1,7 @@
+SELECT
+	CONCAT(m.FirstName,' ', m.LastName),
+	j.Status,
+	j.IssueDate
+FROM Mechanics AS m
+JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+ORDER BY m.MechanicId, j.IssueDate, j.JobId
