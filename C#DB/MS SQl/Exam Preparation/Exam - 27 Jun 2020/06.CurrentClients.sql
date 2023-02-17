2850cbcb8a790979bc80384a8465d2be6a0d24bc
AleksManolow <amanolov7@gmail.com>
2023-02-17 11:21:37 +0200

    Add 06. Current Clients

---
+++ C#DB/MS SQl/Exam Preparation/Exam - 27 Jun 2020/06.CurrentClients.sql
@@ -0,0 +1,8 @@
+SELECT
+	CONCAT(c.FirstName, ' ', c.LastName) AS Client,
+	DATEDIFF(DAY, j.IssueDate, '2017-04-24') AS [Days going],
+	j.Status
+FROM Clients AS c
+JOIN Jobs AS j ON j.ClientId = c.ClientId
+WHERE NOT (j.Status = 'Finished')
+ORDER BY [Days going] DESC, c.ClientId
