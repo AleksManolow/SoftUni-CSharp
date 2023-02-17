685893ff0b35036d3d448b3d7ce2e20463a450c8
AleksManolow <amanolov7@gmail.com>
2023-02-17 11:38:25 +0200

    Add 07. Mechanic Performance

---
+++ C#DB/MS SQl/Exam Preparation/Exam - 27 Jun 2020/07.MechanicPerformance.sql
@@ -0,0 +1,7 @@
+SELECT
+	CONCAT(m.FirstName, ' ', m.LastName) AS Mechanic,
+	AVG(DATEDIFF(DAY, j.IssueDate, j.FinishDate)) AS [Average Days]
+FROM Mechanics AS m
+JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+GROUP BY m.MechanicId, m.FirstName, m.LastName
+ORDER BY m.MechanicId
