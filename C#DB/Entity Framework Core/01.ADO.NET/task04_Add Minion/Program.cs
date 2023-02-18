ab41c2afb8ebab146b181450f25a4fe74cad75fd
AleksManolow <amanolov7@gmail.com>
2023-02-18 17:14:57 +0200

    Add task04.Add Minion

---
+++ C#DB/Entity Framework Core/01.ADO.NET/task04_Add Minion/Program.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace task04_Add_Minion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection =
+                new SqlConnection(@"Server=DESKTOP-AJ5FISA\SQLEXPRESS;Database=MinionsDB;Integrated Security = True;TrustServerCertificate=True;");
+            sqlConnection.Open();
+
+
+            string[] inputRow = Console.ReadLine().Split(": ");
+
+            string[] minionInfo = inputRow[1].Split(' ');
+
+            inputRow = Console.ReadLine().Split(": ");
+
+            string villainName = inputRow[1];
+
+
+            Console.WriteLine(AddNewMinion(sqlConnection, minionInfo, villainName));
+        }
+        private static string AddNewMinion(SqlConnection sqlConnection,
+            string[] minionInfo, string villainName)
+        {
+            StringBuilder output = new StringBuilder();
+
+            string minionName = minionInfo[0];
+            int minionAge = int.Parse(minionInfo[1]);
+            string townName = minionInfo[2];
+
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            try
+            {
+                int townId = GetTownId(sqlConnection, sqlTransaction, output, townName);
+                int villainId = GetVillainId(sqlConnection, sqlTransaction, output, villainName);
+                int minionId = AddMinionAndGetId(sqlConnection, sqlTransaction, minionName, minionAge, townId);
+
+                string addMinionToVillainQuery = @"INSERT INTO [MinionsVillains]([MinionId], [VillainId])
+                                                        VALUES
+                                                        (@MinionId, @VillainId)";
+                SqlCommand addMinionToVillainCmd =
+                    new SqlCommand(addMinionToVillainQuery, sqlConnection, sqlTransaction);
+                addMinionToVillainCmd.Parameters.AddWithValue("@MinionId", minionId);
+                addMinionToVillainCmd.Parameters.AddWithValue("@VillainId", villainId);
+
+                addMinionToVillainCmd.ExecuteNonQuery();
+                output.AppendLine($"Successfully added {minionName} to be minion of {villainName}.");
+
+                sqlTransaction.Commit();
+            }
+            catch (Exception e)
+            {
+                sqlTransaction.Rollback();
+                return e.ToString();
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        private static int GetTownId(SqlConnection sqlConnection, SqlTransaction sqlTransaction, StringBuilder output, string townName)
+        {
+            string townIdQuery = @"SELECT [Id]
+                                     FROM [Towns]
+                                    WHERE [Name] = @TownName";
+            SqlCommand townIdCmd = new SqlCommand(townIdQuery, sqlConnection, sqlTransaction);
+            townIdCmd.Parameters.AddWithValue("@TownName", townName);
+
+            object townIdObj = townIdCmd.ExecuteScalar();
+            if (townIdObj == null)
+            {
+                string addTownQuery = @"INSERT INTO [Towns]([Name])
+                                             VALUES
+                                                    (@TownName)";
+                SqlCommand addTownCmd = new SqlCommand(addTownQuery, sqlConnection, sqlTransaction);
+                addTownCmd.Parameters.AddWithValue("@TownName", townName);
+
+                addTownCmd.ExecuteNonQuery();
+
+                output.AppendLine($"Town {townName} was added to the database.");
+
+                townIdObj = townIdCmd.ExecuteScalar();
+            }
+
+            return (int)townIdObj;
+        }
+
+        private static int GetVillainId(SqlConnection sqlConnection, SqlTransaction sqlTransaction,
+            StringBuilder output, string villainName)
+        {
+            string villainIdQuery = @"SELECT [Id]
+                                          FROM [Villains]
+                                         WHERE [Name] = @VillainName";
+            SqlCommand villainIdCmd = new SqlCommand(villainIdQuery, sqlConnection, sqlTransaction);
+            villainIdCmd.Parameters.AddWithValue("@VillainName", villainName);
+
+            object villainIdObj = villainIdCmd.ExecuteScalar();
+            if (villainIdObj == null)
+            {
+                string evilnessFactorQuery = @"SELECT [Id]
+                                                     FROM [EvilnessFactors]
+                                                    WHERE [Name] = 'Evil'";
+                SqlCommand evilnessFactorCmd =
+                    new SqlCommand(evilnessFactorQuery, sqlConnection, sqlTransaction);
+                int evilnessFactorId = (int)evilnessFactorCmd.ExecuteScalar();
+
+                string insertVillainQuery = @"INSERT INTO [Villains]([Name], [EvilnessFactorId])
+                                                     VALUES
+                                                (@VillainName, @EvilnessFactorId)";
+                SqlCommand insertVillainCmd =
+                    new SqlCommand(insertVillainQuery, sqlConnection, sqlTransaction);
+                insertVillainCmd.Parameters.AddWithValue("@VillainName", villainName);
+                insertVillainCmd.Parameters.AddWithValue("@EvilnessFactorId", evilnessFactorId);
+
+                insertVillainCmd.ExecuteNonQuery();
+                output.AppendLine($"Villain {villainName} was added to the database.");
+
+                villainIdObj = villainIdCmd.ExecuteScalar();
+            }
+
+            return (int)villainIdObj;
+        }
+
+        private static int AddMinionAndGetId(SqlConnection sqlConnection, SqlTransaction sqlTransaction,
+            string minionName, int minionAge, int townId)
+        {
+            string addMinionQuery = @"INSERT INTO [Minions]([Name], [Age], [TownId])
+                                               VALUES 
+                                            (@MinionName, @MinionAge, @TownId)";
+            SqlCommand addMinionCmd = new SqlCommand(addMinionQuery, sqlConnection, sqlTransaction);
+            addMinionCmd.Parameters.AddWithValue("@MinionName", minionName);
+            addMinionCmd.Parameters.AddWithValue("@MinionAge", minionAge);
+            addMinionCmd.Parameters.AddWithValue("@TownId", townId);
+
+            addMinionCmd.ExecuteNonQuery();
+
+            string addedMinionIdQuery = @"SELECT [Id]
+                                       FROM [Minions]
+                                      WHERE [Name] = @MinionName AND [Age] = @MinionAge AND [TownId] = @TownId";
+            SqlCommand getMinionIdCmd = new SqlCommand(addedMinionIdQuery, sqlConnection, sqlTransaction);
+            getMinionIdCmd.Parameters.AddWithValue("@MinionName", minionName);
+            getMinionIdCmd.Parameters.AddWithValue("@MinionAge", minionAge);
+            getMinionIdCmd.Parameters.AddWithValue("@TownId", townId);
+
+            int minionId = (int)getMinionIdCmd.ExecuteScalar();
+
+            return minionId;
+        }
+    }
+
+}
