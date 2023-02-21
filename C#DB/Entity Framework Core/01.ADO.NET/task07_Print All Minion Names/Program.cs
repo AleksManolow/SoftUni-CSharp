7c451189e9d339dfef2cd1bf4b7ca2f1708fbd79
AleksManolow <amanolov7@gmail.com>
2023-02-21 12:20:34 +0200

    Add task07_Print All Minion Names

---
+++ C#DB/Entity Framework Core/01.ADO.NET/task07_Print All Minion Names/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace task07_Print_All_Minion_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection =
+                new SqlConnection(@"Server=DESKTOP-AJ5FISA\SQLEXPRESS;Database=MinionsDB;Integrated Security = True;TrustServerCertificate=True;");
+            sqlConnection.Open();
+
+            string getMinionsNameQuery = @"SELECT Name FROM Minions";
+            SqlCommand getMinionsNameCmd = new SqlCommand(getMinionsNameQuery, sqlConnection);
+
+            SqlDataReader reader = getMinionsNameCmd.ExecuteReader();
+            List<string> minionsName = new List<string>();
+            while (reader.Read())
+            {
+                minionsName.Add((string)reader["Name"]);
+            }
+
+            int firstIt = 0;
+            int secondIt = minionsName.Count() - 1;    
+            while (firstIt < secondIt)
+            {
+                Console.WriteLine(minionsName[firstIt]);
+                Console.WriteLine(minionsName[secondIt]);
+
+
+                if (firstIt + 1 == secondIt)
+                {
+                    break;  
+                }
+
+                firstIt++;
+                secondIt--;
+                if (firstIt == secondIt)
+                {
+                    Console.WriteLine(minionsName[firstIt]);
+                }
+            }
+        }
+    }
+}
