3c4cce800c6ba57de2e67e0d65549e1164ef657b
AleksManolow <amanolov7@gmail.com>
2023-02-23 21:10:40 +0200

    Add 04. Employees with Salary Over 50 000

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(context);
+
+            Console.WriteLine(result);
+            
+        }
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var highSalaryEmployees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    FirstName = e.FirstName,
+                    Salary = e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            foreach (var employee in highSalaryEmployees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
