7cdc108a096ad9212bf8dcfb8388a52f2053d67c
AleksManolow <amanolov7@gmail.com>
2023-02-23 17:12:15 +0200

    Add 03. Employees Full Information

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni
+{
+    using Microsoft.EntityFrameworkCore;
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var allEmployees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.MiddleName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToList();
+
+            foreach (var employee in allEmployees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString();
+        }
+    }
+}
