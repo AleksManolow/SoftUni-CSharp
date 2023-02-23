e4f0e2fd10baf25ed96a66b9df3e65b2075c03fd
AleksManolow <amanolov7@gmail.com>
2023-02-23 21:53:50 +0200

    Add 05. Employees from Research and Development

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesFromResearchAndDevelopment(context);
+
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+           var employees = context
+                .Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - {employee.Salary:F2}");
+            }
+            return sb.ToString().TrimEnd();   
+        }
+    }
+}
