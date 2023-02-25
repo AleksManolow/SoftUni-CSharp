dcd61107f076e4084f65c35f3faea28ac6a204d3
AleksManolow <amanolov7@gmail.com>
2023-02-25 21:25:03 +0200

    Add 11. Find Latest 10 Projects

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = GetLatestProjects(context);
+            Console.WriteLine(result);
+        }
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var projects = context
+                .Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.StartDate,
+                    p.Description
+                })
+                .ToList();
+            foreach (var project in projects)
+            {
+                sb.AppendLine(project.Name);
+                sb.AppendLine(project.Description);
+                sb.AppendLine(project.StartDate.ToString("M/d/yyyy h:mm:ss tt"));    
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
