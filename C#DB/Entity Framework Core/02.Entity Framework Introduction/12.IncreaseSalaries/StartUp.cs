eb4a4440cd9ec2f2e0e3b8cfd5ceb3329a6e6830
AleksManolow <amanolov7@gmail.com>
2023-02-25 21:43:41 +0200

    Add 12. Increase Salaries

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/12.IncreaseSalaries/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = IncreaseSalaries(context);
+            Console.WriteLine(result);
+
+        }
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            var employees = context
+                .Employees
+                .Where(e => e.Department.Name == "Engineering" ||
+                            e.Department.Name == "Tool Design" ||
+                            e.Department.Name == "Marketing" ||
+                            e.Department.Name == "Information Services ")
+                .OrderBy(employee => employee.FirstName)
+                .ThenBy(employee => employee.LastName)
+                .ToList();
+            foreach (var employee in employees)
+            {
+                employee.Salary *= (decimal)1.12; 
+            }
+            context.SaveChanges();
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} ({employee.Salary:F2})");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
