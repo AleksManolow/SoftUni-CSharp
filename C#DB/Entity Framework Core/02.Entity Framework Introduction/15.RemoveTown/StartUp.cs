95593bc762cbfe1457842c8e9fec9ca067b93bf0
AleksManolow <amanolov7@gmail.com>
2023-02-26 21:18:42 +0200

    Add 15. Remove Town

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/15.RemoveTown/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Linq;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = RemoveTown(context);
+            Console.WriteLine(result);
+        }
+        public static string RemoveTown(SoftUniContext context)
+        {
+            Town townForDelete = context.Towns.FirstOrDefault(t => t.Name == "Seattle");
+
+            var address = context.Addresses.Where(a => a.Town.Name == "Seattle").ToList();
+
+            foreach (var employee in context.Employees)
+            {
+                if (address.Any(a => a.AddressId == employee.AddressId))
+                {
+                    employee.AddressId = null;
+                }
+            }
+
+            int numberOfDeletedAddresses = address.Count;
+
+            context.Addresses.RemoveRange(address);
+            context.Towns.Remove(townForDelete);
+
+            context.SaveChanges();
+            return $"{numberOfDeletedAddresses} addresses in Seattle were deleted";
+        }
+    }
+}
