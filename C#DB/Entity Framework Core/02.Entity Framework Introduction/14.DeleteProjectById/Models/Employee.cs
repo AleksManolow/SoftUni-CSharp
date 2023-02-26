45fadf246e0ea9a851deb361753c5dcc0bd55cd5
AleksManolow <amanolov7@gmail.com>
2023-02-26 21:04:20 +0200

    Add 14. Delete Project by Id

---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/14.DeleteProjectById/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SoftUni.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            Departments = new HashSet<Department>();
+            EmployeesProjects = new HashSet<EmployeeProject>();
+            InverseManager = new HashSet<Employee>();
+        }
+
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string JobTitle { get; set; } = null!;
+        public int DepartmentId { get; set; } 
+        public int? ManagerId { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+        public int? AddressId { get; set; }
+
+        public virtual Address? Address { get; set; }
+        public virtual Department Department { get; set; } = null!;
+        public virtual Employee? Manager { get; set; }
+        public virtual ICollection<Department> Departments { get; set; }
+        public virtual ICollection<EmployeeProject> EmployeesProjects { get; set; }
+        public virtual ICollection<Employee> InverseManager { get; set; }
+    }
+}
---
+++ C#DB/Entity Framework Core/02.Entity Framework Introduction/14.DeleteProjectById/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = DeleteProjectById(context);
+            Console.WriteLine(result);
+        }
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            StringBuilder output = new StringBuilder();
+
+            Project projectToDelete = context
+                .Projects.Find(2);
+
+            var referencedProjects = context
+                .EmployeesProjects
+                .Where(ep => ep.ProjectId == projectToDelete.ProjectId)
+                .ToList();
+
+            context.EmployeesProjects.RemoveRange(referencedProjects);
+            context.Projects.Remove(projectToDelete);
+            context.SaveChanges();
+
+            string[] projectNames = context
+                .Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToArray();
+
+            foreach (var p in projectNames)
+            {
+                output.AppendLine(p);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
