ef2f1b665e7853d484c17ced9a07fe7432b0da3d
AleksManolow <amanolov7@gmail.com>
2023-03-05 20:29:36 +0200

    Add 01. Book Shop Database

---
+++ C#DB/Entity Framework Core/05.Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => @"Server=DESKTOP-AJ5FISA\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
