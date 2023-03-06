2ea1f7f3c755b6bdde85fbd7cd15fe686a357d33
AleksManolow <amanolov7@gmail.com>
2023-03-06 12:03:11 +0200

    Add 02. Age Restriction

--- C#DB/Entity Framework Core/05.Advanced Querying/BookShop/StartUp.cs
+++ C#DB/Entity Framework Core/05.Advanced Querying/BookShop/StartUp.cs
@@ -1,7 +1,9 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,6 +11,26 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            string result = GetBooksByAgeRestriction(db, input);
+            Console.WriteLine(result);
+
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var books = context.Books
+                .ToArray()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+            return sb.ToString().TrimEnd();
         }
     }
 }
