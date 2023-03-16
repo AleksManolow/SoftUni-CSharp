4bfb18cf17c7ac73a281b5292103dbb4d732fbd6
AleksManolow <amanolov7@gmail.com>
2023-03-16 14:42:35 +0200

    Add 10. Import Parts

---
+++ C#DB/Entity Framework Core/06.JSON/CarDealer/CarDealer/DTOs/Import/PartDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Import
+{
+    [JsonObject]
+    public class PartDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = null!;
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+        [JsonProperty("supplierId")]
+        public int SupplierId { get; set; }
+    }
+}
--- C#DB/Entity Framework Core/06.JSON/CarDealer/CarDealer/StartUp.cs
+++ C#DB/Entity Framework Core/06.JSON/CarDealer/CarDealer/StartUp.cs
@@ -13,11 +13,15 @@ namespace CarDealer
         {
             CarDealerContext dbContext = new CarDealerContext();
 
-            string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
-            string result = ImportSuppliers(dbContext, inputJson);
+            /*string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            string result = ImportSuppliers(dbContext, inputJson);*/
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/parts.json");
+            string result = ImportParts(dbContext, inputJson);
 
             Console.WriteLine(result);
         }
+        //Task09
         public static string ImportSuppliers(CarDealerContext context, string inputJson)
         {
             IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
@@ -31,6 +35,29 @@ namespace CarDealer
             context.AddRange(suppliers);
             context.SaveChanges();
             return $"Successfully imported {suppliers.Count}.";
+        }
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+
+            var partsDtos = JsonConvert.DeserializeObject<PartDto[]>(inputJson);
+            
+            ICollection<Part> parts = new HashSet<Part>();
+            foreach (var partDto in partsDtos)
+            {
+                if (!context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+                Part part = mapper.Map<Part>(partDto);
+                parts.Add(part);
+            }
+            context.AddRange(parts);
+            context.SaveChanges();
+            return $"Successfully imported {parts.Count}.";
         }
     }
 }
