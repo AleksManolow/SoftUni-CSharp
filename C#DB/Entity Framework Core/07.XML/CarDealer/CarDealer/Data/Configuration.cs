cd366c6f3d32dd5aa25bda0ba6566b88b02dec95
AleksManolow <amanolov7@gmail.com>
2023-03-17 12:09:22 +0200

    Create DB CarDealer

---
+++ C#DB/Entity Framework Core/07.XML/CarDealer/CarDealer/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = 
+            @"Server=DESKTOP-AJ5FISA\SQLEXPRESS;Database=CarDealer;Integrated Security = True;TrustServerCertificate=True;";
+    }
+}
