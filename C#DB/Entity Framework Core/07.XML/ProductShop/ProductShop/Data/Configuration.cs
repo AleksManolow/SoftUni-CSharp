fdce9c9b4dbb30ebbed27b85520551a80c77ddda
AleksManolow <amanolov7@gmail.com>
2023-03-20 18:32:07 +0200

    Create Database Product Shop

---
+++ C#DB/Entity Framework Core/07.XML/ProductShop/ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-AJ5FISA\SQLEXPRESS;Database=ProductShop;Integrated Security = True;TrustServerCertificate=True;";
+    }
+}
