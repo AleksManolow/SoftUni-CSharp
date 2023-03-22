88f681f7c4cb4d6203f433cf4fd245c3a54bf1e8
AleksManolow <amanolov7@gmail.com>
2023-03-22 23:24:17 +0200

    Add 03. Data Export

---
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 06 August 2022/Footballers/DataProcessor/ExportDto/ExportFootballerDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Footballer")]
+    public class ExportFootballerDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+        [XmlElement("Position")]
+        public string Position { get; set; } = null!;
+    }
+}
--- C#DB/Entity Framework Core/Exam Preparation/Exam - 06 August 2022/Footballers/DataProcessor/Serializer.cs
+++ C#DB/Entity Framework Core/Exam Preparation/Exam - 06 August 2022/Footballers/DataProcessor/Serializer.cs
@@ -1,17 +1,74 @@
 ﻿namespace Footballers.DataProcessor
 {
     using Data;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Linq;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            throw new NotImplementedException();
+            var coaches = context.Coaches
+                .Where(c => c.Footballers.Count >= 1)
+                .Select(c => new ExportCoachDto()
+                {
+                    FootballersCount = c.Footballers.Count,
+                    CoachName = c.Name,
+                    Footballers = c.Footballers.Select(f => new ExportFootballerDto()
+                    {
+                        Name = f.Name,
+                        Position = f.PositionType.ToString(),
+                    })
+                    .OrderBy(f => f.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.CoachName)
+                .ToArray();
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, null);
+            var serializer = new XmlSerializer(typeof(ExportCoachDto[]), new XmlRootAttribute("Coaches"));
+            using var writer = new StringWriter();
+            serializer.Serialize(writer, coaches, namespaces);
+
+            return writer.ToString();
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var exportTeams = context
+                .Teams
+                .Where(t => t.TeamsFootballers.Any(tf => tf.Footballer.ContractStartDate >= date))
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Footballers = t.TeamsFootballers
+                        .Where(tf => tf.Footballer.ContractStartDate >= date)
+                        .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                        .ThenBy(tf => tf.Footballer.Name)
+                        .ToArray()
+                        .Select(tf => new
+                        {
+                            FootballerName = tf.Footballer.Name,
+                            ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                            ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                            BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                            PositionType = tf.Footballer.PositionType.ToString()
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Count())
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(exportTeams, Formatting.Indented);
         }
     }
 }
