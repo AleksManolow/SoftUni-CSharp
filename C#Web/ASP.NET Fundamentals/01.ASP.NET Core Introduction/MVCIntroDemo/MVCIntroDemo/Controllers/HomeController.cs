6299aeb2dc3fca39ed6d37159366a1ff9a0dd98b
AleksManolow <amanolov7@gmail.com>
2023-05-29 00:37:15 +0300

    Add part of the MVC Intro Demo

---
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models;
+using System.Diagnostics;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // How to pass date from controller to the view
+            // 1. Using model -> Large structured date, usually forms and details pages
+            // 2. Using ViewBag -> Random date using dynamic object
+            // 3. Using ViewDate -> Random date using dictionary
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVC app.";
+            return View();
+        }
+        public IActionResult Numbers()
+        {
+            return View();
+        }
+        public IActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private IEnumerable<ProductViewModel> _products
+            = new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.12
+                },
+                new ProductViewModel()
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50
+                },
+                new ProductViewModel()
+                {
+                    Id = 3,
+                    Name = "Bread",
+                    Price = 1.50
+                }
+
+            };
+        public IActionResult All()
+        {
+            return View(_products);
+        }
+        public IActionResult ById(int id)
+        {
+            var product = _products
+              .FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+    }
+}
