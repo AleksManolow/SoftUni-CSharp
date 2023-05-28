b8693ad281a56acd4feddc7ca7140fc16a1e98c2
AleksManolow <amanolov7@gmail.com>
2023-05-29 00:38:03 +0300

    Add file

--- C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -38,8 +38,7 @@ namespace MVCIntroDemo.Controllers
         }
         public IActionResult ById(int id)
         {
-            var product = _products
-              .FirstOrDefault(p => p.Id == id);
+            var product = this._products.FirstOrDefault(p => p.Id == id);
             if (product == null)
             {
                 return BadRequest();
