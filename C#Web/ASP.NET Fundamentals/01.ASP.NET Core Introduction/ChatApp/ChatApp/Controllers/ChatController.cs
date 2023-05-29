86b0048c7d44057d144b68653e22f353bb96abc9
AleksManolow <amanolov7@gmail.com>
2023-05-29 16:46:39 +0300

    Add ChatApp

---
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,36 @@
+﻿using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> s_messages = new();
+        public IActionResult Show()
+        {
+            if (!s_messages.Any())
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = s_messages
+                    .Select(m => new MessageViewModel()
+                    {
+                        Sender = m.Key,
+                        MessageText = m.Value
+                    })
+                    .ToList()
+            };
+
+            return View(chatModel);
+        }
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            s_messages.Add(new(chat.CurrentMessage.Sender, chat.CurrentMessage.MessageText));
+            return RedirectToAction(nameof(Show));
+        }
+    }
+}
---
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/ChatApp/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Models.Message
+{
+    public class MessageViewModel
+    {
+        public string Sender { get; set; } = null!;
+        public string MessageText { get; set; } = null!;
+    }
+}
