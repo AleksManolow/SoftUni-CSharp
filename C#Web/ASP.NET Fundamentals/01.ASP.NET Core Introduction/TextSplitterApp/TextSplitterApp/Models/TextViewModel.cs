0cda252da3d7db7d95d87321804848880f4439ec
AleksManolow <amanolov7@gmail.com>
2023-05-30 21:50:33 +0300

    Add changes

--- C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace TextSplitterApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models
 {
     public class TextViewModel
     {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
         public string Text { get; set; } = null!;
         public string SplitText { get; set; } = null!;
     }
