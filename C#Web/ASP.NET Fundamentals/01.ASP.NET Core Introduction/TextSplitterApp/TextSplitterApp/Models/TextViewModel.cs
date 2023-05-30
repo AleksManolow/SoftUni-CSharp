ef29ee34263f1fad703e5e8a3203b1f981aeaafc
AleksManolow <amanolov7@gmail.com>
2023-05-30 21:49:17 +0300

    Add Text Splitter App

---
+++ C#Web/ASP.NET Fundamentals/01.ASP.NET Core Introduction/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TextSplitterApp.Models
+{
+    public class TextViewModel
+    {
+        public string Text { get; set; } = null!;
+        public string SplitText { get; set; } = null!;
+    }
+}
