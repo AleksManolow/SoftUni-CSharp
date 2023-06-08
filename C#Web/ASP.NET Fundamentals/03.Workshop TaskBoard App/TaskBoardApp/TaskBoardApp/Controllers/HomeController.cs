27d3a45b587a7c9722b20b8d84b1480e499bc6a0
AleksManolow <amanolov7@gmail.com>
2023-06-08 21:24:10 +0300

    Add changess

--- C#Web/ASP.NET Fundamentals/03.Workshop TaskBoard App/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
+++ C#Web/ASP.NET Fundamentals/03.Workshop TaskBoard App/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace TaskBoardApp.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		public HomeController() 
+		{ 
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
 		}
 
 		public IActionResult Index()
@@ -18,15 +16,5 @@ namespace TaskBoardApp.Controllers
 			return View();
 		}
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
 	}
 }
