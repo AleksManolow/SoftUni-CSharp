ad0472814b4a65f3fa57cb604b4db7020b386a29
AleksManolow <amanolov7@gmail.com>
2023-06-14 12:08:12 +0300

    Add update-databese in project

---
+++ C#Web/ASP.NET Fundamentals/Exam Preparation/Exam - 22 October 2022/Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
+{
+	public class IdentityUserBook
+	{
+		[ForeignKey(nameof(Collector))]
+		public string CollectorId { get; set; } = null!;
+		public IdentityUser Collector { get; set; } = null!;
+
+		[ForeignKey(nameof(Book))]
+		public int BookId { get; set; }
+		public Book Book { get; set; } = null!;
+	}
+}
