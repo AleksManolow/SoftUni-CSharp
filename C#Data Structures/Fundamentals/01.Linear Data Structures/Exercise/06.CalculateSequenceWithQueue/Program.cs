35532d50c40ac88c486d538ef64545bc7f025ad4
AleksManolow <amanolov7@gmail.com>
2023-10-17 11:40:38 +0300

    Add task06  Calculate Sequence with a Queue

---
+++ C#Data Structures/Fundamentals/01.Linear Data Structures/Exercise/06.CalculateSequenceWithQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.CalculateSequenceWithQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(n);
+            int x, y, z;
+            for (int i = 0; i < 50; i++)
+            {   
+                int temp = q.Dequeue();
+                Console.Write(temp + " ");
+                q.Enqueue(temp + 1);
+                q.Enqueue(2 * temp + 1);
+                q.Enqueue(temp + 2);
+            }
+            
+        }
+    }
+}
