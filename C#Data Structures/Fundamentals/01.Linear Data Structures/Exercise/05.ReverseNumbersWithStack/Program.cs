c2ab7a117b21d3c436f3546fde4d135b843caf1f
AleksManolow <amanolov7@gmail.com>
2023-10-17 11:12:27 +0300

    Add task 05 Reverse Numbers with a Stack

---
+++ C#Data Structures/Fundamentals/01.Linear Data Structures/Exercise/05.ReverseNumbersWithStack/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.ReverseNumbersWithStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            ReverseNumbers(numbers);
+        }
+
+        private static void ReverseNumbers(int[] numbers)
+        {
+            Stack<int> revNumbers = new Stack<int>();
+            foreach (var item in numbers)
+            {
+                revNumbers.Push(item);
+            }
+            Console.WriteLine(string.Join(" ", revNumbers));
+        }
+    }
+}
