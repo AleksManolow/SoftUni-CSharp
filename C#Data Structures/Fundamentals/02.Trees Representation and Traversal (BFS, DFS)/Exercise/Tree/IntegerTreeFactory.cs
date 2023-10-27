baf6f4c0592bf574646bd45d1474e3c7af8718f1
AleksManolow <amanolov7@gmail.com>
2023-10-27 13:53:20 +0300

    Add skeleton

---
+++ C#Data Structures/Fundamentals/02.Trees Representation and Traversal (BFS, DFS)/Exercise/Tree/IntegerTreeFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class IntegerTreeFactory
+    {
+        private Dictionary<int, IntegerTree> nodesByKey;
+
+        public IntegerTreeFactory()
+        {
+            this.nodesByKey = new Dictionary<int, IntegerTree>();
+        }
+
+        public IntegerTree CreateTreeFromStrings(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntegerTree CreateNodeByKey(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEdge(int parent, int child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntegerTree GetRoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
